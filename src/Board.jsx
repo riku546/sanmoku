6ce87d17c25b63f4e7b39894eefccd2e88f406c0
riku <riku62560@gmail.com>
2024-04-25 21:23:12 +0900

    勝利判定以外出来た。

--- src/Board.jsx
+++ src/Board.jsx
@@ -10,43 +10,53 @@ function Board() {
 
   const [board, setBoard] = useState(defaultBoard);
   const [turn, setTurn] = useState(1);
+  const [winner, setWinner] = useState(0);
+
+  const winnerCheck = (board) => {
+    const point = [];
+    let winner = 0;
+    board.forEach((row) => {
+      row.forEach((i) => {
+        point.push(i);
+        if (point[0] === point[1] && point[0] === point[2]) {
+          winner = point[0];
+        }
+      });
+    });
+    return winner;
+  };
+
+  const clickHandler = (y, x) => {
+    const newBoard = [...board];
+    newBoard[y][x] = turn;
+    setBoard(newBoard);
+    const winner = winnerCheck(newBoard);
+    console.log(winner);
+    setWinner(winner);
+    setTurn((prev) => 3 - prev);
+  };
 
   return (
-    <div className="board">
-      {/* {board.map((row) => {
-        row.map((i) => {
-          console.log(i);
+    <>
+      <h3>{winner !== 0 ? "winner: " + winner : "turn: " + turn}</h3>
 
-          <div className="cell" key={row}>
-            <div className="stone" key={i}>
-              <h1>1</h1>
-            </div>
-          </div>;
-        });
-      })} */}
-      {board.map((row, index) => {
-        {row.map((i) => {
-         
-          return <li>{i}</li>
-          
-        })};
-      })}
-    </div>
+      <div className="board">
+        {board.map((row, rowIndex) => (
+          <div key={rowIndex} className="row">
+            {row.map((cell, cellIndex) => (
+              <div
+                key={cellIndex}
+                className="cell"
+                onClick={() => clickHandler(rowIndex, cellIndex)}
+              >
+                <div className="stone">{cell === 0 ? "" : cell}</div>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
   );
-
-  // return (
-  //   <div className="board">
-  //     {board.map((row, rowIndex) => (
-  //       <div key={rowIndex} className="row">
-  //         {row.map((cell, cellIndex) => (
-  //           <div key={cellIndex} className="cell">
-  //             <div className="stone">{cell === 0 ? "" : cell}</div>
-  //           </div>
-  //         ))}
-  //       </div>
-  //     ))}
-  //   </div>
-  // );
 }
 
 export default Board;
